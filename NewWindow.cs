254e8a47d8299c51fe0c1244b971ee5dd90d2dd3
Admin <Admin@HOME-PC>
2021-03-29 07:10:04 +1000

    NewWindow

--- NewWindow.cs
+++ NewWindow.cs
@@ -4,14 +4,19 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Point = Avalonia.Point;
 using Rectangle = Avalonia.Controls.Shapes.Rectangle;
 
 namespace PrimSCADA
 {
     public class NewWindow : Window
     {
+        private Rectangle RectangleBoundWindow;
         private PointerPoint? _PointerPoint;
+        private PointerPoint PointerPointRectangleBoundWindow;
         private PixelPoint _PixelPoint;
+        private Point PointRectangleBoundWindow;
+        private bool IsLeftClickRectangleBoundWindow;
         private void InputElement_OnPointerPressed(object? sender, PointerPressedEventArgs e)
         {
             _PointerPoint = e.GetCurrentPoint(null);
@@ -46,7 +51,7 @@ namespace PrimSCADA
             Close();
         }
 
-        private void StyledElement_OnInitialized(object? sender, EventArgs e)
+        private void NewWindow_OnInitialized(object? sender, EventArgs e)
         {
             Screens screens = new Window().Screens;
             PixelRect pr = screens.Primary.Bounds;
@@ -56,8 +61,47 @@ namespace PrimSCADA
 
         private void RectangleBound_OnPointerMoved(object? sender, PointerEventArgs e)
         {
-            PointerPoint pp = e.GetCurrentPoint(this.FindControl<Rectangle>("RectangleBound"));
-            ;
+            Rect rect = RectangleBoundWindow.Bounds;
+            PointRectangleBoundWindow = e.GetCurrentPoint(RectangleBoundWindow).Position;
+            if (PointRectangleBoundWindow.Y > RectangleBoundWindow.StrokeThickness && PointRectangleBoundWindow.X == 0)
+            {
+                Cursor = new Cursor(StandardCursorType.LeftSide);
+                if (IsLeftClickRectangleBoundWindow)
+                {
+                    RectangleBoundWindow.
+                }
+            }
+            else if (PointRectangleBoundWindow.X == rect.Width - 1 &&
+                     PointRectangleBoundWindow.Y > RectangleBoundWindow.StrokeThickness)
+                Cursor = new Cursor(StandardCursorType.RightSide);
+        }
+
+        private void TopLevel_OnOpened(object? sender, EventArgs e)
+        {
+            RectangleBoundWindow = this.FindControl<Rectangle>("RectangleBound");
+        }
+
+        private void RectangleBound_OnPointerLeave(object? sender, PointerEventArgs e)
+        {
+            Cursor = new Cursor(StandardCursorType.Arrow);
+        }
+
+        private void RectangleBound_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+        {
+            Rect rect = RectangleBoundWindow.Bounds;
+            PointRectangleBoundWindow = e.GetCurrentPoint(RectangleBoundWindow).Position;
+            if (PointRectangleBoundWindow.Y > RectangleBoundWindow.StrokeThickness && PointRectangleBoundWindow.X == 0)
+            {
+                IsLeftClickRectangleBoundWindow = true;
+            }
+            else if (PointRectangleBoundWindow.X == rect.Width - 1 &&
+                     PointRectangleBoundWindow.Y > RectangleBoundWindow.StrokeThickness)
+                Cursor = new Cursor(StandardCursorType.RightSide);
+        }
+
+        private void RectangleBound_OnPointerReleased(object? sender, PointerReleasedEventArgs e)
+        {
+            IsLeftClickRectangleBoundWindow = false;
         }
     }
 }
